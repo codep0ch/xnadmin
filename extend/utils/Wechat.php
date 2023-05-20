b209cee64ce79f31789376417cdb64ed0cf7c03b
codepoch <osmands@live.com>
2023-05-20 15:53:38 +0800

    Update Wechat.php

--- extend/utils/Wechat.php
+++ extend/utils/Wechat.php
@@ -12,14 +12,14 @@ class Wechat
         $merchantId = '1644958700';
 
         // 从本地文件中加载「商户API私钥」，「商户API私钥」会用来生成请求的签名
-        $merchantPrivateKeyFile = 'file://'.app()->getRootPath().'extend/utils/cert/apiclient_cert.pem';
+        $merchantPrivateKeyFile = 'file://'.app()->getRootPath().'/extend/utils/cert/apiclient_key.pem';
         $merchantPrivateKeyInstance = Rsa::from($merchantPrivateKeyFile, Rsa::KEY_TYPE_PRIVATE);
 
         // 「商户API证书」的「证书序列号」
         $merchantCertificateSerial = '5244495AA3C4C59788075C7892C116C1BAF2A61C';
 
         // 从本地文件中加载「微信支付平台证书」，用来验证微信支付应答的签名
-        $platformCertificateFilePath = 'file://'.app()->getRootPath().'extend/utils/cert/apiclient_cert.pem';
+        $platformCertificateFilePath = 'file://'.app()->getRootPath().'/extend/utils/cert/wechatpay.pem';
         $platformPublicKeyInstance = Rsa::from($platformCertificateFilePath, Rsa::KEY_TYPE_PUBLIC);
 
         // 从「微信支付平台证书」中获取「证书序列号」
