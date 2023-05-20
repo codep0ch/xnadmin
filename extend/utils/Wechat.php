22fec6e6438265fd4b92b5a2fd5ef1016f044ac4
codepoch <osmands@live.com>
2023-05-20 15:58:08 +0800

    Update Wechat.php

--- extend/utils/Wechat.php
+++ extend/utils/Wechat.php
@@ -11,14 +11,14 @@ class Wechat
         // 商户号
         $merchantId = '1644958700';
 
-        // 从本地文件中加载「商户API私钥」，「商户API私钥」会用来生成请求的签名
+        // 从本地文件中加载「商户API私钥」，「商户API私钥」会用来生成请求的签名(这里的文件采用微信支付API安全中生成的证书key)
         $merchantPrivateKeyFile = 'file://'.app()->getRootPath().'/extend/utils/cert/apiclient_key.pem';
         $merchantPrivateKeyInstance = Rsa::from($merchantPrivateKeyFile, Rsa::KEY_TYPE_PRIVATE);
 
-        // 「商户API证书」的「证书序列号」
+        // 「商户API证书」的「证书序列号」 (微信支付API安全中获取的证书序列号)
         $merchantCertificateSerial = '40BC765B67DC5C7872AC1D8C9F76F601770C3CC0';
 
-        // 从本地文件中加载「微信支付平台证书」，用来验证微信支付应答的签名
+        // 从本地文件中加载「微信支付平台证书」，用来验证微信支付应答的签名 (这里的文件采用使用CertificateDownloader生成的证书)
         $platformCertificateFilePath = 'file://'.app()->getRootPath().'/extend/utils/cert/wechatpay.pem';
         $platformPublicKeyInstance = Rsa::from($platformCertificateFilePath, Rsa::KEY_TYPE_PUBLIC);
 
@@ -38,9 +38,7 @@ class Wechat
     }
     public function test(){
         // 发送请求
-        $resp = $this->instance->chain('v3/certificates')->get(
-            ['debug' => true] // 调试模式，https://docs.guzzlephp.org/en/stable/request-options.html#debug
-        );
+        $resp = $this->instance->chain('v3/certificates')->get();
         echo $resp->getBody();
     }
 }
