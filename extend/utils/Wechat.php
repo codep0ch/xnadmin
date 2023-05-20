653653a2336617f1ef3f3772b8a817f73c5268b7
codepoch <osmands@live.com>
2023-05-20 17:01:52 +0800

    Update Wechat.php

--- extend/utils/Wechat.php
+++ extend/utils/Wechat.php
@@ -8,29 +8,33 @@ class Wechat
     public $instance = null;
     public function createWechatPay($merchantId, $merchantPrivateKeyFile, $merchantCertificateSerial, $platformCertificateFilePath): Wechat
     {
-        // 从本地文件中加载「商户API私钥」，「商户API私钥」会用来生成请求的签名(这里的文件采用微信支付API安全中生成的证书key)
-        $merchantPrivateKeyFile = 'file://'.$merchantPrivateKeyFile;
-        $merchantPrivateKeyInstance = Rsa::from($merchantPrivateKeyFile, Rsa::KEY_TYPE_PRIVATE);
+        try {
+            // 从本地文件中加载「商户API私钥」，「商户API私钥」会用来生成请求的签名(这里的文件采用微信支付API安全中生成的证书key)
+            $merchantPrivateKeyFile = 'file://'.$merchantPrivateKeyFile;
+            $merchantPrivateKeyInstance = Rsa::from($merchantPrivateKeyFile, Rsa::KEY_TYPE_PRIVATE);
 
-        // 「商户API证书」的「证书序列号」 (微信支付API安全中获取的证书序列号)
-        $merchantCertificateSerial = empty($merchantCertificateSerial) ?? 0;
+            // 「商户API证书」的「证书序列号」 (微信支付API安全中获取的证书序列号)
+            $merchantCertificateSerial = empty($merchantCertificateSerial) ?? 0;
 
-        // 从本地文件中加载「微信支付平台证书」，用来验证微信支付应答的签名 (这里的文件采用使用CertificateDownloader生成的证书)
-        $platformCertificateFilePath = 'file://'.$platformCertificateFilePath;
-        $platformPublicKeyInstance = Rsa::from($platformCertificateFilePath, Rsa::KEY_TYPE_PUBLIC);
+            // 从本地文件中加载「微信支付平台证书」，用来验证微信支付应答的签名 (这里的文件采用使用CertificateDownloader生成的证书)
+            $platformCertificateFilePath = 'file://'.$platformCertificateFilePath;
+            $platformPublicKeyInstance = Rsa::from($platformCertificateFilePath, Rsa::KEY_TYPE_PUBLIC);
 
-        // 从「微信支付平台证书」中获取「证书序列号」
-        $platformCertificateSerial = PemUtil::parseCertificateSerialNo($platformCertificateFilePath);
+            // 从「微信支付平台证书」中获取「证书序列号」
+            $platformCertificateSerial = PemUtil::parseCertificateSerialNo($platformCertificateFilePath);
 
-        // 构造一个 APIv3 客户端实例
-        $this->instance = Builder::factory([
-            'mchid'      => $merchantId,
-            'serial'     => $merchantCertificateSerial,
-            'privateKey' => $merchantPrivateKeyInstance,
-            'certs'      => [
-                $platformCertificateSerial => $platformPublicKeyInstance,
-            ],
-        ]);
+            // 构造一个 APIv3 客户端实例
+            $this->instance = Builder::factory([
+                'mchid'      => $merchantId,
+                'serial'     => $merchantCertificateSerial,
+                'privateKey' => $merchantPrivateKeyInstance,
+                'certs'      => [
+                    $platformCertificateSerial => $platformPublicKeyInstance,
+                ],
+            ]);
+        }catch (\Exception $e){
+            $this->instance = new \stdClass();
+        }
         return $this;
     }
 
