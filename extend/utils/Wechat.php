7c525362432a25cd08a7730e5c3665fd1a56e3d8
codepoch <osmands@live.com>
2023-05-21 18:37:15 +0800

    Update Wechat.php

--- extend/utils/Wechat.php
+++ extend/utils/Wechat.php
@@ -11,37 +11,28 @@ class Wechat
     public $error = [];
     public function createWechatPay($merchantId, $merchantPrivateKeyFile, $merchantCertificateSerial, $platformCertificateFilePath): Wechat
     {
-        try {
-            // 从本地文件中加载「商户API私钥」，「商户API私钥」会用来生成请求的签名(这里的文件采用微信支付API安全中生成的证书key)
-            $merchantPrivateKeyFile = 'file://'.$merchantPrivateKeyFile;
-            $merchantPrivateKeyInstance = Rsa::from($merchantPrivateKeyFile, Rsa::KEY_TYPE_PRIVATE);
+        // 从本地文件中加载「商户API私钥」，「商户API私钥」会用来生成请求的签名(这里的文件采用微信支付API安全中生成的证书key)
+        $merchantPrivateKeyFile = 'file://'.$merchantPrivateKeyFile;
+        $merchantPrivateKeyInstance = Rsa::from($merchantPrivateKeyFile, Rsa::KEY_TYPE_PRIVATE);
 
-            // merchantCertificateSerial:「商户API证书」的「证书序列号」 (微信支付API安全中获取的证书序列号)
+        // merchantCertificateSerial:「商户API证书」的「证书序列号」 (微信支付API安全中获取的证书序列号)
 
-            // 从本地文件中加载「微信支付平台证书」，用来验证微信支付应答的签名 (这里的文件采用使用CertificateDownloader生成的证书)
-            $platformCertificateFilePath = 'file://'.$platformCertificateFilePath;
-            $platformPublicKeyInstance = Rsa::from($platformCertificateFilePath, Rsa::KEY_TYPE_PUBLIC);
+        // 从本地文件中加载「微信支付平台证书」，用来验证微信支付应答的签名 (这里的文件采用使用CertificateDownloader生成的证书)
+        $platformCertificateFilePath = 'file://'.$platformCertificateFilePath;
+        $platformPublicKeyInstance = Rsa::from($platformCertificateFilePath, Rsa::KEY_TYPE_PUBLIC);
 
-            // 从「微信支付平台证书」中获取「证书序列号」
-            $platformCertificateSerial = PemUtil::parseCertificateSerialNo($platformCertificateFilePath);
+        // 从「微信支付平台证书」中获取「证书序列号」
+        $platformCertificateSerial = PemUtil::parseCertificateSerialNo($platformCertificateFilePath);
 
-            // 构造一个 APIv3 客户端实例
-            $this->instance = Builder::factory([
-                'mchid'      => $merchantId,
-                'serial'     => $merchantCertificateSerial,
-                'privateKey' => $merchantPrivateKeyInstance,
-                'certs'      => [
-                    $platformCertificateSerial => $platformPublicKeyInstance,
-                ],
-            ]);
-        } catch (\Exception $e) {
-            $this->instance = new \stdClass();
-            if ($e instanceof \GuzzleHttp\Exception\RequestException && $e->hasResponse()) {
-                $this->error[] = $e->getResponse()->getBody();
-            }else{
-                $this->error[] = $e->getMessage();
-            }
-        }
+        // 构造一个 APIv3 客户端实例
+        $this->instance = Builder::factory([
+            'mchid'      => $merchantId,
+            'serial'     => $merchantCertificateSerial,
+            'privateKey' => $merchantPrivateKeyInstance,
+            'certs'      => [
+                $platformCertificateSerial => $platformPublicKeyInstance,
+            ],
+        ]);
         return $this;
     }
 
@@ -63,12 +54,7 @@ class Wechat
             $array = json_decode($resp->getBody(), true);
             return !empty($array['data'][0]['serial_no']);
         } catch (\Exception $e) {
-            if ($e instanceof \GuzzleHttp\Exception\RequestException && $e->hasResponse()) {
-                var_dump($e->getResponse());
-                $this->error[] = $e->getResponse()->getBody();
-            }else{
-                $this->error[] = $e->getMessage();
-            }
+            $this->error[] = $e->getMessage();
         }
         return false;
     }
