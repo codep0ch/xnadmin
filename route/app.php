b464a3a67abfff31607a36d3494350b3d3baa711
codepoch <osmands@live.com>
2023-05-22 01:25:39 +0800

    Update app.php

--- route/app.php
+++ route/app.php
@@ -1,17 +1,7 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
 use think\facade\Route;
-
 Route::get('think', function () {
     return 'hello,ThinkPHP6!';
 });
 
-Route::get('wechat/auth2', 'wechat/Auth/Auth2');
+Route::any('wechat/auth2', 'wechat/Auth/Auth2');
