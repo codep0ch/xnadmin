a59319abbe5d3b5a04d7354a5dae63cf25b216b9
codepoch <osmands@live.com>
2023-05-21 18:26:54 +0800

    Update Coupon.php

--- app/admin/controller/Coupon.php
+++ app/admin/controller/Coupon.php
@@ -71,7 +71,7 @@ class Coupon extends AdminBase
                     ],
                     'coupon_code_mode' => 'WECHATPAY_MODE'
                 ];
-                $resp = $wechatInstance->chain('v3/marketing/busifavor/stocks')->post(json_encode($postData));
+                $resp = $wechatInstance->chain('v3/marketing/busifavor/stocks')->post($postData);
                 $array = json_decode($resp->getBody(), true);
             }catch (ClientException $exception) {
                 var_dump($exception->getMessage());
