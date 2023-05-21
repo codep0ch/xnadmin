455a7837fc97f0258e56e662568d25292ffb6069
codepoch <osmands@live.com>
2023-05-21 18:01:03 +0800

    Update Wechat.php

--- app/admin/controller/Wechat.php
+++ app/admin/controller/Wechat.php
@@ -34,7 +34,7 @@ class Wechat extends AdminBase
             $wechat_setting_data['merchantCertificateSerial'],
             $wechat_setting_data['platformCertificateFilePath']
         )->sign_test();
-        return view('', ['wechat_setting_data'=>$wechat_setting_data, 'status' => $status]);
+        return view('', ['wechat_setting_data'=>$wechat_setting_data, 'status' => $status == true ? '1' : '2']);
     }
 
     public function uploader(){
