6d05b636175dc7e802c844505e070a1505dcabd1
codepoch <osmands@live.com>
2023-05-28 19:57:10 +0800

    Update app.php

--- app/miniprogram/route/app.php
+++ app/miniprogram/route/app.php
@@ -1,5 +1,10 @@
 <?php
 use think\facade\Route;
 
+
 Route::get('auth', '/wechat/Auth/init');
-Route::get('info', '/wechat/Coupon/init');
+//分组路由与中间件的应用 （多应用模式）
+//group （第一个参数路由组名称 ，）
+Route::group('', function(){
+    Route::get('info', '/wechat/Coupon/init');
+})->middleware(\app\miniprogram\middleware\ApiMiddleware::class);
