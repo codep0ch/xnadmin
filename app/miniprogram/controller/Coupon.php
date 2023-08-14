d81b5130f2e0ffe16bbf2dcf94f8b89aff4afb9f
codepoch <osmands@live.com>
2023-08-14 11:39:54 +0800

    Update Coupon.php

--- app/miniprogram/controller/Coupon.php
+++ app/miniprogram/controller/Coupon.php
@@ -42,7 +42,8 @@ class Coupon extends Base
             if($statusCode != 200){
                 return commonApiReturn(400,[],'微信返回核销失败');
             }else{
-                $couponLog->update(['is_consume' => 1]);
+                $couponLog->is_consume = 1;
+                $couponLog->save();
                 return commonApiReturn(200,[],'核销成功');
             }
         }catch (\Exception $e){
