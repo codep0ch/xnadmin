e43d2a91687dc7476dbc8000b8b3a956b49295ba
codepoch <osmands@live.com>
2023-08-14 11:23:49 +0800

    Update Coupon.php

--- app/miniprogram/controller/Coupon.php
+++ app/miniprogram/controller/Coupon.php
@@ -22,8 +22,8 @@ class Coupon extends Base
             $wechat_setting_data['merchantCertificateSerial'],
             $wechat_setting_data['platformCertificateFilePath']
         )->getInstance();
-        $couponLog = CouponSendLog::create()->where(['code' => $coupon_code])->find();
-        if(\app\common\model\Coupon::create()->find($couponLog['couponid'])['status'] != 1){
+        $couponLog = CouponSendLog::where(['code' => $coupon_code])->find();
+        if(\app\common\model\Coupon::find($couponLog['couponid'])['status'] != 1){
             return commonApiReturn(400,[],'券禁止核销');
         }
         try {
