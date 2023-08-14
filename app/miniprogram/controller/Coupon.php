a987f2c9bac2d391fb07df603d49758358c20584
codepoch <osmands@live.com>
2023-08-14 12:18:23 +0800

    Update Coupon.php

--- app/miniprogram/controller/Coupon.php
+++ app/miniprogram/controller/Coupon.php
@@ -25,22 +25,21 @@ class Coupon extends Base
         }
 
         try {
-        //创建微信实例
-        $wechat_setting_data = WechatSettingModel::find(1);
-        $wechatInstance = (new \utils\Wechat())->createWechatPay(
-            $wechat_setting_data['merchantId'],
-            $wechat_setting_data['merchantPrivateKeyFile'],
-            $wechat_setting_data['merchantCertificateSerial'],
-            $wechat_setting_data['platformCertificateFilePath']
-        )->getInstance();
+            //创建微信实例
+            $wechat_setting_data = WechatSettingModel::find(1);
+            $wechatInstance = (new \utils\Wechat())->createWechatPay(
+                $wechat_setting_data['merchantId'],
+                $wechat_setting_data['merchantPrivateKeyFile'],
+                $wechat_setting_data['merchantCertificateSerial'],
+                $wechat_setting_data['platformCertificateFilePath']
+            )->getInstance();
 
-            $resp = $wechatInstance->v3->marketing->busifavor->users->_openid_->coupons->_coupon_code_->appids->_appid_->getAsync([
+            $resp = $wechatInstance->v3->marketing->busifavor->users->_openid_->coupons->_coupon_code_->appids->_appid_->get([
                 // 变量名 => 变量值
                 'openid' => $couponLog['open_id'],
                 'coupon_code' => $coupon_code,
                 'appid' => $wechat_setting_data['wechatAppId']
             ]);
-            var_dump($resp);
         //$resp = $wechatInstance->chain("v3/marketing/busifavor/users/{$couponLog['open_id']}/coupons/{$coupon_code}/appids/{$wechat_setting_data['wechatAppId']}")->get();
         } catch (\Exception $e) {
                 // 进行错误处理
