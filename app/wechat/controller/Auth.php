618e19440b97d7c23a96a79f02ced2db6854bf39
codepoch <osmands@live.com>
2023-08-14 11:07:21 +0800

    Update Auth.php

--- app/wechat/controller/Auth.php
+++ app/wechat/controller/Auth.php
@@ -20,6 +20,7 @@ class Auth extends Base
             'couponid' => $id,
             'out_request_no' => $params['out_request_no'],
             'open_id' => $params['open_id'],
+            'create_time' => time()
         ]);
         $params['sign'] = $this->getSignV2($params, 'RL6VHZ1DG78N5Y4X1S9FP6QK0U345790');
         $url = 'https://action.weixin.qq.com/busifavor/getcouponinfo?'.http_build_query($params).'#wechat_redirect';
